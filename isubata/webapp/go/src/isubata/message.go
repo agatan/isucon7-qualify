167d6f5d73606ab56f3e9daaf1a624a30632ea69
izumin5210 <masayuki@izumin.info>
2017-10-21 16:04:12 +0900

    Guard nil

--- isubata/webapp/go/src/isubata/message.go
+++ isubata/webapp/go/src/isubata/message.go
@@ -20,11 +20,23 @@ func appendMessageID(m *Message) error {
 func countMessages(chanID int64) (int64, error) {
 	conn := pool.Get()
 	defer conn.Close()
-	return redis.Int64(conn.Do("ZCARD", messageIDsKey(chanID)))
+	cnt, err := redis.Int64(conn.Do("ZCARD", messageIDsKey(chanID)))
+	if err == redis.ErrNil {
+		return 0, nil
+	} else if err == nil {
+		return cnt, nil
+	}
+	return 0, nil
 }
 
 func countUnreadMessages(chanID, lastSeenMsgID int64) (int64, error) {
 	conn := pool.Get()
 	defer conn.Close()
-	return redis.Int64(conn.Do("ZREVRANK", messageIDsKey(chanID), lastSeenMsgID))
+	cnt, err := redis.Int64(conn.Do("ZREVRANK", messageIDsKey(chanID), lastSeenMsgID))
+	if err == redis.ErrNil {
+		return 0, nil
+	} else if err == nil {
+		return cnt, nil
+	}
+	return 0, nil
 }
