84fb23a6a0d763d379d6fc64e906cfa0e79619b1
izumin5210 <masayuki@izumin.info>
2017-10-21 17:37:41 +0900

    Stop using prepared statement

--- isubata/webapp/go/src/isubata/app.go
+++ isubata/webapp/go/src/isubata/app.go
@@ -71,7 +71,7 @@ func init() {
 		redis_url = "redis://localhost:6379"
 	}
 
-	dsn := fmt.Sprintf("%s%s@tcp(%s:%s)/isubata?parseTime=true&loc=Local&charset=utf8mb4",
+	dsn := fmt.Sprintf("%s%s@tcp(%s:%s)/isubata?parseTime=true&loc=Local&charset=utf8mb4&interpolateParams=true",
 		db_user, db_password, db_host, db_port)
 
 	log.Printf("Connecting to db: %q", dsn)
