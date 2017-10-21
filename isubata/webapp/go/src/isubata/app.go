04fa1209ca6bddc86b56a604d2f249d9ca473903
izumin5210 <masayuki@izumin.info>
2017-10-21 14:25:47 +0900

    Flush redis on initialize

--- isubata/webapp/go/src/isubata/app.go
+++ isubata/webapp/go/src/isubata/app.go
@@ -221,7 +221,9 @@ func getInitialize(c echo.Context) error {
 	db.MustExec("DELETE FROM image WHERE id > 1001")
 	db.MustExec("DELETE FROM channel WHERE id > 10")
 	db.MustExec("DELETE FROM message WHERE id > 10000")
-	db.MustExec("DELETE FROM haveread")
+	conn := pool.Get()
+	defer conn.Close()
+	conn.Do("FLUSHALL")
 	return c.String(204, "")
 }
 
